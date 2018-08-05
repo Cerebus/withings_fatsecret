db9b5531d1b457412278c93ff83c6ee4079ebb9c
T. Miller <cerebus2@gmail.com>
2018-08-05 13:09:14 -0500

    structured credentials file

--- flask_listener.py
+++ flask_listener.py
@@ -6,16 +6,12 @@ app = Flask(__name__)
 
 lb_to_kg = 0.45359237
 
-with open('credentials.yml') as f:
+with open('../fs_credentials.yml') as f:
         config = yaml.load(f)
 
 @app.route('/', methods=['POST'])
 def api_root():
-    conn = fs.Fatsecret(
-        consumer_key=config['consumer_key'],
-        consumer_secret=config['consumer_secret'],
-        session_token=(config['user_token'], config['user_secret'])
-        )
+    conn = fs.Fatsecret(**config)
     return str(conn.weight_update(request.get_json()['weightkg']))
 
 
